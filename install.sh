c640feb4695a22bb5d2467f4d66427d4cb840cd7
bannedbook <banned.ebook@gmail.com>
2022-04-27 21:44:29 +1000

    update

--- install.sh
+++ install.sh
@@ -136,7 +136,7 @@ install_XrayR() {
     chmod +x XrayR
     mkdir /etc/XrayR/ -p
     rm /etc/systemd/system/XrayR.service -f
-    file="https://github.com/bannedbook/XrayR/raw/master/XrayR.service"
+    file="https://github.com/bannedbook/XrayR/raw/main/XrayR.service"
     wget -q -N --no-check-certificate -O /etc/systemd/system/XrayR.service ${file}
     #cp -f XrayR.service /etc/systemd/system/
     systemctl daemon-reload
@@ -177,7 +177,7 @@ install_XrayR() {
     if [[ ! -f /etc/XrayR/ruelist ]]; then
         cp ruelist /etc/XrayR/
     fi
-    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/bannedbook/XrayR/master/XrayR.sh
+    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/bannedbook/XrayR/main/XrayR.sh
     chmod +x /usr/bin/XrayR
     ln -s /usr/bin/XrayR /usr/bin/xrayr # 小写兼容
     chmod +x /usr/bin/xrayr
